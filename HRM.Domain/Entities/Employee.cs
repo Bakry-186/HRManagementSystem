748043e6f9f4053423ea9d2de2ab0c4399a33fae
bakry56 <abdobakry823@gmail.com>
2026-06-14 02:41:27 +0300

    feat(employee): add base entity and employee domain entity

---
+++ HRM.Domain/Entities/Employee.cs
@@ -0,0 +1,13 @@
+namespace HRM.Domain.Entities;
+
+public class Employee : BaseEntity
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Email { get; set; }
+    public string? PhoneNumber { get; set; }
+    public required DateOnly HireDate { get; set; }
+    public required string JobTitle { get; set; }
+    public required decimal Salary { get; set; }
+    public bool IsActive { get; set; } = true;
+}
