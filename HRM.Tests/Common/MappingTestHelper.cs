6e4d7241b7deb4e6903da59070891d0fc6969e53
bakry56 <abdobakry823@gmail.com>
2026-06-15 04:24:55 +0300

    test(department): add unit tests for department handlers

---
+++ HRM.Tests/Common/MappingTestHelper.cs
@@ -0,0 +1,21 @@
+using AutoMapper;
+using HRM.Application.Mappings;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace HRM.Tests.Common;
+
+public static class MappingTestHelper
+{
+    public static IMapper CreateMapper()
+    {
+        var services = new ServiceCollection();
+        services.AddLogging();
+        services.AddAutoMapper(cfg =>
+        {
+            cfg.AddProfile<EmployeeMappingProfile>();
+            cfg.AddProfile<DepartmentMappingProfile>();
+        });
+        return services.BuildServiceProvider().GetRequiredService<IMapper>();
+    }
+}
