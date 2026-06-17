bb8f69c9e5c2e64aa9aa72a0ee4a188163212f9f
bakry56 <abdobakry823@gmail.com>
2026-06-18 01:47:02 +0300

    fix(database): apply filtered unique indexes to allow soft-deleted name and email reuse

---
+++ HRM.Infrastructure/Migrations/20260617224351_UpdateDepartmentAndEmployeeIndexFilterd.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HRM.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDepartmentAndEmployeeIndexFilterd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_Email",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Departments_Name",
+                table: "Departments");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_Email",
+                table: "Employees",
+                column: "Email",
+                unique: true,
+                filter: "[IsActive] = 1");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Departments_Name",
+                table: "Departments",
+                column: "Name",
+                unique: true,
+                filter: "[IsActive] = 1");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_Email",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Departments_Name",
+                table: "Departments");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_Email",
+                table: "Employees",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Departments_Name",
+                table: "Departments",
+                column: "Name",
+                unique: true);
+        }
+    }
+}
